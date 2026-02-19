ef9e01b016bfb5cd4699435a389323d6e5572140
Muhammad-Nawlo <nawlomuhammadit@gmail.com>
2026-02-20 02:53:08 +0300

    Init structure for saas with packages

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    \App\Providers\Filament\LandlordPanelProvider::class,
+    \App\Providers\Filament\TenantPanelProvider::class,
+    App\Providers\TenancyServiceProvider::class,
+];
